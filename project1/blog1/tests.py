94f9ed339ada01f876e7147c43a9f90fe3b00d97
joseph <jsphndukwe@yahoo.com>
2019-03-17 15:47:54 +0100

    fix blog

--- project1/blog1/tests.py
+++ project1/blog1/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Board
 
 # Create your tests here.
+class Blog1TestCase(TestCase):
+    def setUp(self):
+        Board.objects.create(title='hello',pic='')
+        Board.objects.create(title='you',pic='')
+        Board.objects.create(title='why',pic='')
+
+    def testCount(self):
+        return self.assertEqual(Board.objects.all().count(), 1)
+
+    def test1(self):
+        return self.assertEqual(Board.objects.get(title='hello').title, 'hello')
